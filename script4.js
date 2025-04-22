a86e5a417e5c006c621dd1ca7616f7a8a84fdd52
JanneRova <jannexd9@gmail.com>
2025-04-22 11:54:44 +0300

    Delete script4.js

--- script4.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("nappi").addEventListener("click", function() { 
-    let syote = document.getElementById("teksti").value; 
-    document.getElementById("tuloste").textContent = syote; 
-}); 
- 
